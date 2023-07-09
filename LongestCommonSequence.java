e5e1fa8ab61aa873f15dae2b2ce59200b586d633
Ahaan25 <ahaan25092002@gmail.com>
2023-07-09 15:43:44 +0530

    Longest common subsequence

--- LongestCommonSequence.java
+++ LongestCommonSequence.java
@@ -1,9 +1,17 @@
-import java.util.*;
-public class LongestCommonSequence{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-
-        
-        sc.close();
-    }
+public class LongestCommonSequence {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] mn = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    mn[i][j] = mn[i - 1][j - 1] + 1;
+                } else {
+                    mn[i][j] = Math.max(mn[i - 1][j], mn[i][j - 1]);
+                }
+            }
+        }
+        return mn[m][n];
+        }
 }

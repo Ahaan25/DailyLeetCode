e0f7406126e83509e1f974ea485f69a28ceb37db
Ahaan Shaikh <96652099+Ahaan25@users.noreply.github.com>
2023-07-10 16:47:25 +0530

    Two Sum

---
+++ TwoSum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    public static void main(String[] args){
+        int[] a={1, 4, 5, 11, 12};
+        int b=9;
+        twoSum(a, b);
+    }
+}

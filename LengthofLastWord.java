515f24e18963f2394064a441791689fd6b0c6921
Ahaan Shaikh <96652099+Ahaan25@users.noreply.github.com>
2024-01-26 07:19:51 -0800

    Length of Last Word

---
+++ LengthofLastWord.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int len=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)!=' '){
+                while(i>=0 && s.charAt(i) != ' '){
+                    len++;
+                    i--;
+               }
+                break;
+            }
+        }
+        return len;
+    }
+}
